bfaff6a2af716b1de25f45b6b6252ef4c6749c67
owantdelhiver <owenboutet@gmail.com>
2023-04-22 16:43:03 +0200

    liaison entre le bouton add et la page add product categorie

---
+++ src/main/java/com/groupe4/backoffice/servlet/product_categorie/ProductCategorieAddServlet.java
@@ -0,0 +1,17 @@
+package com.groupe4.backoffice.servlet.product_categorie;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/product_categorie/add")
+public class ProductCategorieAddServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/html/product_categorie/add.jsp").forward(req,resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/html/product_categorie/add.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: owenb
+  Date: 22/04/2023
+  Time: 16:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<jsp:include page="/WEB-INF/html/include/head.jsp"/>
+<jsp:include page="/WEB-INF/html/include/nav.jsp"/>
+
+<div class="container mt-5">
+  <h2>Page ajout</h2>
+</div>
+
+<jsp:include page="/WEB-INF/html/include/footer.jsp"/>
