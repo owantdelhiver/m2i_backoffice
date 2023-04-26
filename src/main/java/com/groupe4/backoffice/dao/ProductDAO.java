4e42cec99d1221033f109be07c695c79c129c285
owantdelhiver <owenboutet@gmail.com>
2023-04-26 22:43:41 +0200

    create Class DAO for product

---
+++ src/main/java/com/groupe4/backoffice/dao/ProductDAO.java
@@ -0,0 +1,32 @@
+package com.groupe4.backoffice.dao;
+
+import com.groupe4.backoffice.model.Product;
+
+import java.util.List;
+
+public class ProductDAO implements GenericDAO<Product> {
+    @Override
+    public void create(Product obj) {
+
+    }
+
+    @Override
+    public List<Product> fetchAll() {
+        return null;
+    }
+
+    @Override
+    public Product fetchById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Product obj) {
+
+    }
+
+    @Override
+    public void delete(Product obj) {
+
+    }
+}
