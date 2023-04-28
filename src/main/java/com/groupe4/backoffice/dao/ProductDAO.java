05d2e95f6a283cef0200710f1ae7782300c540b8
owantdelhiver <owenboutet@gmail.com>
2023-04-28 14:54:03 +0200

    function search By id or Name

--- src/main/java/com/groupe4/backoffice/dao/ProductDAO.java
+++ src/main/java/com/groupe4/backoffice/dao/ProductDAO.java
@@ -121,4 +121,35 @@ public class ProductDAO implements GenericDAO<Product> {
             throw new RuntimeException(e);
         }
     }
+
+    public List<Product> fetchByNameOrId(String search) {
+        List<Product> products = new ArrayList<>();
+        try(PreparedStatement preparedStatement = DB.getConnection().prepareStatement("SELECT p.id, p.name, p.price, p.short_description, p.description, p.stock, p.picture_url, pc.id, pc.name FROM product p LEFT JOIN product_category pc on p.id_category = pc.id WHERE p.id LIKE ? OR p.name LIKE ?")) {
+            preparedStatement.setString(1,search);
+            preparedStatement.setString(2, "%" + search + "%");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                products.add(
+                        new Product(
+                                resultSet.getLong("p.id"),
+                                resultSet.getString("p.name"),
+                                resultSet.getFloat("p.price"),
+                                resultSet.getString("p.short_description"),
+                                resultSet.getString("p.description"),
+                                resultSet.getInt("p.stock"),
+                                JsonFormater.JsonToListString(resultSet.getString("p.picture_url")),
+                                new ProductCategory(
+                                        resultSet.getLong("pc.id"),
+                                        resultSet.getString("pc.name")
+                                )
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return products;
+    }
 }
--- src/main/java/com/groupe4/backoffice/service/ProductService.java
+++ src/main/java/com/groupe4/backoffice/service/ProductService.java
@@ -9,11 +9,10 @@ public class ProductService {
     public static List<Product> fetchAll() {
         return new ProductDAO().fetchAll();
     }
-
     public static Product fetchOneById(int id) {
         return new ProductDAO().fetchById((long) id);
     }
-
+    public static List<Product> fetchByNameOrId(String search) {return new ProductDAO().fetchByNameOrId(search);}
     public static void create(Product product) {
         new ProductDAO().create(product);
     }
@@ -22,7 +21,7 @@ public class ProductService {
         new ProductDAO().update(product);
     }
 
-    public static int delete(Product product) {
+    public static void delete(Product product) {
         new ProductDAO().delete(product);
     }
 }
