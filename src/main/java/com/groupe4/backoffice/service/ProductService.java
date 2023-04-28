3ab23e4e191fe44b5d81ceba3297e8f7bb6228da
owantdelhiver <owenboutet@gmail.com>
2023-04-28 14:10:18 +0200

    all default function service

--- src/main/java/com/groupe4/backoffice/service/ProductService.java
+++ src/main/java/com/groupe4/backoffice/service/ProductService.java
@@ -9,4 +9,20 @@ public class ProductService {
     public static List<Product> fetchAll() {
         return new ProductDAO().fetchAll();
     }
+
+    public static Product fetchOneById(int id) {
+        return new ProductDAO().fetchById((long) id);
+    }
+
+    public static void create(Product product) {
+        new ProductDAO().create(product);
+    }
+
+    public static void update(Product product) {
+        new ProductDAO().update(product);
+    }
+
+    public static int delete(Product product) {
+        new ProductDAO().delete(product);
+    }
 }
