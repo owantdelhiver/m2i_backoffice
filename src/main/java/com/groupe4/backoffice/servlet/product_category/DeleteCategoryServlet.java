d0be52927ccd934ca96c75d7631bd45e539f628e
owantdelhiver <owenboutet@gmail.com>
2023-04-28 23:05:18 +0200

    delete category

---
+++ src/main/java/com/groupe4/backoffice/servlet/product_category/DeleteCategoryServlet.java
@@ -0,0 +1,21 @@
+package com.groupe4.backoffice.servlet.product_category;
+
+import com.groupe4.backoffice.model.ProductCategory;
+import com.groupe4.backoffice.service.ProductCategorieService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/delete-category")
+public class DeleteCategoryServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        ProductCategorieService.delete(id);
+        resp.sendRedirect(req.getContextPath() + "/categories");
+    }
+}
