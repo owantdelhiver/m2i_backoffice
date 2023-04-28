4a2b0672d89740e66364027ffef839e93ab3d4c5
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-04-28 16:05:46 +0200

    Creation of method findByCredentials return Class Admin

---
+++ src/main/java/com/groupe4/backoffice/dao/AdminDao.java
@@ -0,0 +1,41 @@
+package com.groupe4.backoffice.dao;
+import com.groupe4.backoffice.model.DB;
+import com.groupe4.backoffice.model.Admin;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminDao {
+
+        private static final String SELECT_ADMIN_BY_EMAIL = "select * from admin where email = ? AND password = ?";
+        public AdminDao() {
+        }
+
+        public Admin findByCredentials(String email, String password) {
+            Admin admin = null;
+            Connection connection = DB.getConnection();
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ADMIN_BY_EMAIL);) {
+
+                preparedStatement.setString(1, email);
+                preparedStatement.setString(2, password);
+
+                ResultSet rs = preparedStatement.executeQuery();
+
+                if (rs.next()) {
+                    String firstname = rs.getString("firstname");
+                    String lastname = rs.getString("lastname");
+
+                    admin = new Admin(firstname, lastname , email, password);
+
+                }
+            } catch (SQLException e) {
+                System.out.println("SQL Exception Select User "+e.getMessage());
+            }
+            return admin;
+        }
+
+
+
+
+}
