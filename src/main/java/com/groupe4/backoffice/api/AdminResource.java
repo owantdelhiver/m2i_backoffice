cea4d5842496cb34beb54d592ed94af26a60acd5
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-04-29 13:41:06 +0200

    created api/admins to return admins list dto

--- src/main/java/com/groupe4/backoffice/api/AdminResource.java
+++ src/main/java/com/groupe4/backoffice/api/AdminResource.java
@@ -1,5 +1,4 @@
 package com.groupe4.backoffice.api;
-
 import com.groupe4.backoffice.dto.AdminDto;
 import com.groupe4.backoffice.service.AdminService;
 import jakarta.ws.rs.GET;
@@ -7,24 +6,18 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
 import java.util.List;
-
-@Path("/users")
+@Path("/admins")
 public class AdminResource {
-private static AdminService adminService = new AdminService();
 
-
-        @GET
-        @Produces(value = MediaType.APPLICATION_JSON)
-        public Response getAll() {
-            List<AdminDto> adminList = adminService.findAll();
-
-            return Response
-                    .ok(adminList)
-                    .build();
-        }
-
-
+    @GET
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        AdminService adminService = new AdminService();
+        List<AdminDto> adminList = adminService.findAll();
+        return Response
+                .ok(adminList)
+                .build();
     }
+
 }
