87ebb1d34d1a59c34d020caba128ba47d5de0f7c
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-02 22:01:11 +0200

    Fetchall method implemented

---
+++ src/main/java/com/groupe4/backoffice/dao/OrderDao.java
@@ -0,0 +1,54 @@
+package com.groupe4.backoffice.dao;
+
+import com.groupe4.backoffice.model.DB;
+import com.groupe4.backoffice.model.order.Order;
+import com.groupe4.backoffice.model.order.OrderStatus;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDao implements GenericDAO<Order> {
+    @Override
+    public List<Order> fetchAll() {
+        Connection connection = DB.getConnection();
+        List<Order> orders = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM `order`"))
+             {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                orders.add(
+                        new Order(
+                               resultSet.getInt("id"),
+                                resultSet.getDate("date"),
+                                OrderStatus.getFromLabel(resultSet.getString("status"))
+                        ));
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return orders;
+    }
+    @Override
+    public void create(Order obj) {
+
+    }
+
+    @Override
+    public Order fetchById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Order obj) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
