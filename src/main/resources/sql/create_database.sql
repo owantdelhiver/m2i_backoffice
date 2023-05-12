752e0413517631777b1df409d6ac51d910003ff4
owantdelhiver <owenboutet@gmail.com>
2023-05-12 12:24:10 +0200

    update script

--- src/main/resources/sql/create_database.sql
+++ src/main/resources/sql/create_database.sql
@@ -30,20 +30,65 @@ CREATE TABLE product(
 );
 
 CREATE TABLE orders(
-                       id INT AUTO_INCREMENT,
-                       date DATE,
-                       status VARCHAR(50),
-                       PRIMARY KEY (id)
+                        id INT AUTO_INCREMENT,
+                        date DATE,
+                        status VARCHAR(50),
+                        id_user INT,
+                        PRIMARY KEY (id),
+                        FOREIGN KEY(id_user) REFERENCES USER(id)
 
 );
 
-CREATE TABLE orderLine
+CREATE TABLE order_line
 (
-                        id INT AUTO_INCREMENT,
-                        id_product INT,
-                        id_order INT,
-                        quantity INT,
-                        PRIMARY KEY (id),
-                        FOREIGN KEY (id_product) REFERENCES product(id),
-                        FOREIGN KEY (id_order) REFERENCES orders(id)
+    id INT AUTO_INCREMENT,
+    id_product INT,
+    id_order INT,
+    quantity INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_product) REFERENCES product(id),
+    FOREIGN KEY (id_order) REFERENCES `order`(id)
+);
+
+CREATE TABLE cart
+(
+    id INT AUTO_INCREMENT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE adress
+(
+    id int AUTO_INCREMENT,
+    street VARCHAR(255),
+    number int,
+    zipcode int,
+    city VARCHAR(255),
+    country VARCHAR(255),
+    extra_info TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE USER
+(
+    id INT AUTO_INCREMENT,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    id_adress INT,
+    id_cart INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_adress) REFERENCES adress(id),
+    FOREIGN KEY (id_cart) REFERENCES cart(id)
+);
+
+CREATE TABLE cart_line
+(
+    id int AUTO_INCREMENT,
+    quantity int,
+    id_cart int,
+    id_product int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_cart) REFERENCES cart(id),
+    FOREIGN KEY (id_product) REFERENCES product(id)
 );
