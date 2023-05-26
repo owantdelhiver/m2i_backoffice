4d4714a982b510f21f04871fb5af5b2f3a590dc0
Cyprien Chasseray <cyprien.chasseray@gmail.com>
2023-05-26 23:25:53 +0200

    added product search bar

--- src/main/java/com/groupe4/backoffice/service/ProductService.java
+++ src/main/java/com/groupe4/backoffice/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.groupe4.backoffice.service;
 
+import com.groupe4.backoffice.dao.ProductCategorieDAO;
 import com.groupe4.backoffice.dao.ProductDAO;
 import com.groupe4.backoffice.model.product.Product;
+import com.groupe4.backoffice.model.product.ProductCategory;
 
 import java.util.List;
 
@@ -9,6 +11,8 @@ public class ProductService {
     public static List<Product> fetchAll() {
         return new ProductDAO().fetchAll();
     }
+    public static List<Product> fetchByIdOrName(String search) { return new ProductDAO().fetchByNameOrId(search);}
+
     public static Product fetchOneById(int id) {
         return new ProductDAO().fetchById((long) id);
     }
