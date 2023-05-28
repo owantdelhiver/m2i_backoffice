2a2109b25d92e4fad58932eb689956b4c3c7f7f4
owantdelhiver <owenboutet@gmail.com>
2023-05-28 17:29:31 +0200

    listing orders

---
+++ src/main/java/com/groupe4/backoffice/servlet/order/OrderServlet.java
@@ -0,0 +1,22 @@
+package com.groupe4.backoffice.servlet.order;
+
+import com.groupe4.backoffice.service.OrderService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/orders")
+
+public class OrderServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("orders", new OrderService().getAll());
+        req
+                .getRequestDispatcher("/WEB-INF/html/orders/list.jsp")
+                .forward(req, resp);
+    }
+}
